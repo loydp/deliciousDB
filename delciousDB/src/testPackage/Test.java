8ec8147a42de451997056c331f51cb29a6bc0b82
Alisa Wallace <Alisa Wallace@192.168.86.47>
2019-11-23 14:12:05 -0800

    Trying another commit.

--- delciousDB/src/testPackage/Test.java
+++ delciousDB/src/testPackage/Test.java
@@ -4,8 +4,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("This is a test");
-        System.out.println("Alisa's Test");
-        System.out.println("Another test?");
     }
 
 }
