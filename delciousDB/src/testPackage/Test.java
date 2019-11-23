8dc7a168ab27dd7ff2598e962b706a8f5b0d1096
Alisa Wallace <Alisa Wallace@192.168.86.47>
2019-11-23 14:06:35 -0800

    Trying again.

--- delciousDB/src/testPackage/Test.java
+++ delciousDB/src/testPackage/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is a test");
         System.out.println("Alisa's Test");
+        System.out.println("Another test?");
     }
 
 }
