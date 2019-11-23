7e0d276b694f44d8294ea97cdc0607f6accfba65
herstadd <herstadd@seattleu.edu>
2019-11-23 14:25:12 -0800

    Commit to avoid loss of a comment

--- delciousDB/src/testPackage/Test.java
+++ delciousDB/src/testPackage/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is a test");
         //Indeed a test!
+        //Middle Test
         System.out.println("Alisa was here!");
     }
 
