20bcaf2179e2ffef856c4229070650b723bd4d46
Owner <Owner@Radius>
2019-11-23 23:52:24 -0800

    !!! Changed main class, to deliciousBusiness. Remember to update build path.

--- delciousDB/src/deliciousDB/QueryJDBC.java
+++ delciousDB/src/deliciousDB/QueryJDBC.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package queryrunner;
+package deliciousDB;
 import java.sql.Connection;
 import java.sql.*;
 import java.sql.DriverManager;
