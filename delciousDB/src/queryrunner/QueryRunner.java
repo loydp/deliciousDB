04d303dcf1e4fdef81529cc66defa9e631d38eef
Owner <Owner@Radius>
2019-11-23 23:16:30 -0800

    Headers on table data

--- delciousDB/src/queryrunner/QueryRunner.java
+++ delciousDB/src/queryrunner/QueryRunner.java
@@ -181,20 +181,12 @@ public class QueryRunner {
     private String m_projectTeamApplication;
     private ArrayList<QueryData> m_queryArray;  
     private int m_updateAmount;
-
+    
+    
     /**
-     * @param args the command line arguments
+     * 
+     * @param args A string array containing the command line arguments.
      */
-    
-    public static void printView(String[][] queryResults) {
-         for (String[] row : queryResults) {
-             for (String field : row) {
-                 System.out.printf("%s ", field);
-             }
-             System.out.println();
-         }
-     }
-    
     public static void main(String[] args) {
         // TODO code application logic here
 
@@ -253,7 +245,6 @@ public class QueryRunner {
                 // functions directly, you can choose to do that. It will be harder, but that is your option.
                 // NOTE - You can look at the QueryRunner API calls that are in QueryFrame.java for assistance. You should not have to 
                 //    alter any code in QueryJDBC, QueryData, or QueryFrame to make this work.
-//                System.out.println("Please write the non-gui functionality");
                 
             	// Need to make a QueryRunner object to use its methods
                 QueryRunner consoleQR = new QueryRunner();
@@ -267,7 +258,6 @@ public class QueryRunner {
                 // (Stored being our DB params kept in constant variables)
                 // Connect - get parameters needed for Connect() function
                 Scanner keyboard = new Scanner(System.in);
-                
                 
                 System.out.println("CONNECTING TO DATABASE ...");
                 // System.out.print("Enter hostname: ");
@@ -292,7 +282,7 @@ public class QueryRunner {
                 
                 
                 // Create some kind of help menu 
-                // Show commands: 
+                // Show commands:
                 // - Show queries (Number, name, params)
                 // - Run single query (based on number)
                 // - Run all queries (use loop to go through all)
@@ -303,25 +293,61 @@ public class QueryRunner {
                 // E.g., each query has its own array of param values, then we
                 // just loop through the array to input all params for a given query
                 
-                
                 // ExecuteQuery must take an array of parameters and will get
                 // the length of that array in QueryJDBC.ExecuteQuery
                 // If there are no params, create an array size 0 and pass in
+                
+                
+                
+                
+                
+                
                 String[] noParams = new String[0];
                 
                 // 0 refers to the query number (index) in the queryArray
                 consoleQR.ExecuteQuery(0, noParams);
+                
+                // Gathers table header and field data.
+                String[] queryHeaders = consoleQR.m_jdbcData.GetHeaders();
                 String[][] queryResults = consoleQR.m_jdbcData.GetData();
                 
-                printView(queryResults);
+                // Prints table header and field data.
+                printView(queryHeaders, queryResults);
                 
                 
-                // SHUT DOWN
                 
+                
+                // end line.
                 keyboard.close();               
+                // TODO figure out why disconnect throws an error.
                 // consoleQR.Disconnect();
             }
         }
- 
-    }    
+        
+    }   
+    
+    
+
+    /**
+     * Displays the results of queries, first using attribute names and
+     * then using table fields.
+     * @param queryResults
+     */
+    public static void printView( String[] queryHeaders, 
+                                  String[][] queryResults) {
+   
+        for (String attribute : queryHeaders) {
+            System.out.printf("|  %-20s ", attribute);
+        }
+        
+        System.out.println();
+  
+        for (String[] row : queryResults) {
+            for (String field : row) {
+                System.out.printf("|  %-20s", field);
+            }
+            System.out.println();
+        }
+    }
+      
 }
